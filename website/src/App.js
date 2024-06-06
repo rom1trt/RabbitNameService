ad665d135d9c52e14007d7e5c56955de648434dc
djunice <djunice.lumban@epita.fr>
2024-06-06 11:35:49 +0200

    fix: modified better App.js

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [message, setMessage] = useState("Hello World");
 
+  useEffect(() => {
+    console.log("Component mounted");
+  }, []);
+
+  const handleClick = () => {
+    setMessage("Button Clicked");
+  };
+
   return (
     <div className="App">
       <h1>{message}</h1>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 };
