67c6a51a037249b5b1f6e1511a4f9bc805744725
Lina Farchado <lina.farchado@epita.fr>
2024-06-06 11:45:44 +0200

    feat(app): display input value

--- website/src/App.js
+++ website/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [message, setMessage] = useState("Hello World");
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
     console.log("Component mounted");
@@ -11,10 +12,17 @@ const App = () => {
     setMessage("Button Clicked");
   };
 
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+
   return (
     <div className="App">
       <h1>{message}</h1>
       <button onClick={handleClick}>Click Me</button>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <p>{inputValue}</p>
     </div>
   );
 };
